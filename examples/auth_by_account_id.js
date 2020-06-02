4a7167f8116556d7f9dfeea631938cfc277e47ed
MobiusTraderLtd <it@mobius-soft.org>
2020-06-02 09:03:35 +0300

    Method ApiTraderAuth

--- examples/auth_by_account_id.js
+++ examples/auth_by_account_id.js
@@ -4,15 +4,14 @@ const config = require('./config');
 async function run() {
   const mt7 = await MobiusTrader.getInstance(config);
 
-  const login = 'test@example.com';
-  const password = 'testPassword';
+  const accountId = 123;
   const clientIP = '10.11.12.13';
   const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36';
 
   try {
-    const { JWT, AccountId } = await mt7.traderAuth(login, password, clientIP, userAgent);
+    const jwt = await mt7.getJWT(accountId, clientIP, userAgent);
 
-    mt7.log(`JWT: ${JWT} AccountId: ${AccountId}`);
+    mt7.log(`JWT: ${jwt}`);
   } catch (e) {
     console.error('Error:', e);
   }
